77d441beacca6df7980eb651c1acfb5058e81e74
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-07-18 17:35:43 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
+const cors = require('cors')
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => res.send('hello world'));
 
