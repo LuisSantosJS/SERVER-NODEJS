f9988f5e66029e4ac4c87f7d7a40010678ad5e8a
Luis Santos <dasilvasantosluisfelipe@gmail.com>
2020-07-18 22:02:35 -0500

    initial commit

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const routes = express.Router()
-
-
-routes.get('/', (req, res) => res.send('olá'))
+routes.get('/', (req, res) => res.send('olá'));
 
 module.exports = routes
