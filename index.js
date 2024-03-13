c1fb7ef4bb0135cc911c7469004893ad167ada21
Luis Santos <65205217+LuisSantosJS@users.noreply.github.com>
2024-03-13 01:10:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen( process.env.PORT || 3000, ()=> console.log('server running'));
+app.listen( process.env.PORT || 3333, ()=> console.log('server running'));
